42c5183c06ab32188ec02832d2d60bfc5963fc8a
Carston Work <carston.work@gmail.com>
2021-07-26 12:13:07 -0600

    initial commit, added helloWorld file

---
+++ helloWorld.js
@@ -0,0 +1,15 @@
+// This is our first JavaScript comment
+
+// Here is some random text
+
+let randoText = 'monkey!'
+
+let igloo = 5;
+
+if (igloo === 5) {
+    console.log("Hooray!");
+}
+
+if (igloo == 5) [
+    console.log("Hooray - ish????")
+]
